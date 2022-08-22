2fb75e483433a3ea6927b30f56382bc21c8c4765
mougomougo <mougomougo@126.com>
2022-08-22 23:27:41 +0800

    完善了Web界面框架结构,实现了新增端口重定向配置页面

---
+++ web/static/js/indirect_config_new.js
@@ -0,0 +1,10 @@
+// 加载form 和 element模块
+layui.use(['form', 'element'], function(){
+    var form = layui.form
+    ,element = layui.element
+
+    // 点击新增配置时
+    form.on('submit(indirect_cfg_new)', function(data) {
+
+    });
+});
