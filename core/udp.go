a65eff67f318ddb0f19a06ab7f9e7d480f21a0e6
mougomougo <mougomougo@126.com>
2022-09-21 22:15:00 +0800

    修改UDP网络模型。 修改后性能有少许下降。 由于UDP没有连接的概念，因此无法感知对端状态，这就造成协程可能不能及时关闭。 要避免此情况，要尽量使用黑白名单，防止恶意的其他连接。

--- core/udp.go
+++ core/udp.go
@@ -3,14 +3,11 @@ package core
 import (
 	"errors"
 	"net"
-	"os"
 	"pintd/config"
 	"pintd/filter"
 	"pintd/plog"
-	"runtime"
 	"strings"
 	"sync"
-	"time"
 )
 
 type ConnInfo struct {
@@ -18,18 +15,18 @@ type ConnInfo struct {
 	Conn *net.UDPConn
 }
 
-// because UDP dont't have connection state. so we cannot know remote is or not closed.
-// and it cause we cannot timely close expire goroutine when using one conn one goroutine model.
 func HandleUdpConn(listener Listener, cfg *config.RedirectConfig, wg *sync.WaitGroup) {
 	defer wg.Done()
 	defer listener.udpconn.Close()
 
 	var (
 		conns sync.Map
-		queue = NewDgramQueue()
+		rconn *net.UDPConn
+		buf   = make([]byte, 65535)
 		raddr = net.UDPAddr{IP: net.ParseIP(cfg.RemoteAddr), Port: cfg.RemorePort}
 	)
 
+	// close all conn.
 	defer func() {
 		conns.Range(func(key, value any) bool {
 			rconn := value.(ConnInfo).Conn
@@ -38,30 +35,13 @@ func HandleUdpConn(listener Listener, cfg *config.RedirectConfig, wg *sync.WaitG
 		})
 	}()
 
-	// read right multi server data to message queue.
-	go ReadMultiServer(listener.udpconn, queue, &conns)
-
-	// handle write data from message queue.
-	go Write(listener.udpconn, queue)
-
-	// read left multi client data to message queue.
-	if err := ReadMultiClient(listener.udpconn, queue, &raddr, &conns, cfg); err != nil {
-		plog.Println("Error : %s, Udp Listener %s Closed.", err.Error(),
-			listener.udpconn.LocalAddr().String())
-		return
-	}
-}
-
-func ReadMultiClient(lconn *net.UDPConn, queue *DgramQueue, dial *net.UDPAddr, conns *sync.Map, cfg *config.RedirectConfig) error {
-	var (
-		rconn *net.UDPConn
-		buf   = make([]byte, 65536)
-	)
-
+	// read left multi client data.
 	for {
-		n, laddr, err := lconn.ReadFromUDP(buf)
+		n, laddr, err := listener.udpconn.ReadFromUDP(buf)
 		if err != nil {
-			return err
+			plog.Println("Error : %s, Udp Listener %s Closed.", err.Error(),
+				listener.udpconn.LocalAddr().String())
+			return
 		}
 
 		// filter address
@@ -71,11 +51,12 @@ func ReadMultiClient(lconn *net.UDPConn, queue *DgramQueue, dial *net.UDPAddr, c
 			continue
 		}
 
+		// new connection.
 		val, ok := conns.Load(laddr.String())
 		if ok {
 			rconn = val.(ConnInfo).Conn
 		} else {
-			rconn, err = net.DialUDP("udp", nil, dial)
+			rconn, err = net.DialUDP("udp", nil, &raddr)
 			if err != nil {
 				plog.Println("DialUDP Failed : %s", err.Error())
 				continue
@@ -84,68 +65,39 @@ func ReadMultiClient(lconn *net.UDPConn, queue *DgramQueue, dial *net.UDPAddr, c
 			conninfo := ConnInfo{Addr: laddr, Conn: rconn}
 			conns.Store(laddr.String(), conninfo)
 
+			go UdpRightToLeft(listener.udpconn, rconn, laddr, &conns)
+
 			plog.Println("New UDP Redirect Connection from [%s]->[%s] redirect to [%s]->[%s].",
-				laddr.String(), lconn.LocalAddr().String(),
+				laddr.String(), listener.udpconn.LocalAddr().String(),
 				rconn.LocalAddr().String(), rconn.RemoteAddr().String())
 		}
 
-		PutDgramToQueue(queue, buf, n, rconn, dial)
-	}
-}
-
-func ReadMultiServer(lconn *net.UDPConn, queue *DgramQueue, conns *sync.Map) {
-	loop := true
-	buf := make([]byte, 65536)
-
-	for loop {
-		conns.Range(func(key, value any) bool {
-			rconn := value.(ConnInfo).Conn
-			laddr := value.(ConnInfo).Addr
-
-			rconn.SetReadDeadline(time.Now().Add(time.Microsecond * 1))
-			n, err := rconn.Read(buf)
-			if err != nil && !errors.Is(err, os.ErrDeadlineExceeded) {
-				conns.Delete(key)
-				rconn.Close()
-				plog.Println("Error : %s On UDP Redirect Connection from [%s]->[%s] redirect to [%s]->[%s].",
-					err.Error(), laddr.String(), lconn.LocalAddr().String(),
-					rconn.LocalAddr().String(), rconn.RemoteAddr().String())
-				loop = false
-				return false
-			}
-
-			if !errors.Is(err, os.ErrDeadlineExceeded) {
-				PutDgramToQueue(queue, buf, n, lconn, laddr)
-			}
-
-			return true
-		})
-
-		runtime.Gosched()
-	}
-}
-
-func Write(lconn *net.UDPConn, queue *DgramQueue) {
-	var (
-		err error
-		buf = make([]byte, 65536)
-	)
-
-	for {
-		// we don't need lock, because no race data.
-		conn, addr, n := GetDgramFromQueue(queue, buf)
-
 		// UDP don't have write buffer, write will not blocking.
 		// if send failed, datadgram is lost.
-		if conn == lconn {
-			_, err = conn.WriteToUDP(buf[:n], addr) // send left
-		} else {
-			_, err = conn.Write(buf[:n]) // send right
-		}
+		rconn.Write(buf[:n])
+	}
+}
 
+func UdpRightToLeft(lconn, rconn *net.UDPConn, laddr *net.UDPAddr, conns *sync.Map) {
+	buf := make([]byte, 65536)
+
+	defer conns.Delete(laddr.String())
+	defer rconn.Close()
+
+	for {
+		n, err := rconn.Read(buf)
 		if err != nil {
-			plog.Println("Write Udp : %s", err.Error())
+			// conn closed
+			if errors.Is(err, net.ErrClosed) {
+				return
+			}
+
+			plog.Println("Error : %s On UDP Redirect Connection from [%s]->[%s] redirect to [%s]->[%s].",
+				err.Error(), laddr.String(), lconn.LocalAddr().String(),
+				rconn.LocalAddr().String(), rconn.RemoteAddr().String())
 			return
 		}
+
+		lconn.WriteToUDP(buf[:n], laddr)
 	}
 }
