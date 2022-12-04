6e97d8cf93d5f654ff1a4f206fb3a8d777b54a4e
memleaker <memleak@126.com>
2022-12-04 23:57:28 +0800

    1. 设置TCP Keepalive 2. 修复资源泄漏的BUG 3. 修改连接远端时逻辑, 使其不阻塞接收连接

--- core/tcp.go
+++ core/tcp.go
@@ -14,19 +14,15 @@ import (
 )
 
 func HandleTcpConn(listener Listener, cfg *config.RedirectConfig, wg *sync.WaitGroup) {
-	var (
-		lconn net.Conn
-		rconn net.Conn
-		err   error
-		ch    = make(chan bool, cfg.MaxRedirects)
-	)
 
 	defer wg.Done()
 	defer listener.listener.Close()
 
+	ch := make(chan bool, cfg.MaxRedirects)
+
 	// Wait Connection coming.
 	for {
-		lconn, err = listener.listener.Accept()
+		lconn, err := listener.listener.Accept()
 		if err != nil {
 			plog.Println("Accept Connection Failed %s, listener closed.", err.Error())
 			return
@@ -44,28 +40,37 @@ func HandleTcpConn(listener Listener, cfg *config.RedirectConfig, wg *sync.WaitG
 			lconn.RemoteAddr().String())
 
 		// Dial to remote.
-		rconn, err = net.DialTimeout("tcp", cfg.RemoteAddr+":"+strconv.Itoa(cfg.RemorePort),
-			time.Second*time.Duration(3))
-		if err != nil {
-			lconn.Close()
-			plog.Println("Tcp Dial Failed : %s", err.Error())
-			continue
-		}
-
-		// Dial Success.
-		// check connections number.
-		select {
-		case ch <- true:
-		default:
-			lconn.Close()
-			plog.Println("Connection Limit to %d, Closed Connection.", cfg.MaxRedirects)
-			continue
-		}
-		plog.Println("Tcp Dial to %s Success.", rconn.RemoteAddr().String())
-
-		// handle data.
-		go HandleTcpData(lconn, rconn, ch)
+		go DialToRemote(lconn, cfg, ch)
 	}
+}
+
+func DialToRemote(lconn net.Conn, cfg *config.RedirectConfig, ch chan bool) {
+	rconn, err := net.DialTimeout("tcp", cfg.RemoteAddr+":"+strconv.Itoa(cfg.RemorePort),
+		time.Second*time.Duration(3))
+	if err != nil {
+		lconn.Close()
+		plog.Println("Tcp Dial Failed : %s", err.Error())
+		return
+	}
+
+	plog.Println("Tcp Dial to %s Success.", rconn.RemoteAddr().String())
+
+	// check connections number.
+	select {
+	case ch <- true:
+	default:
+		lconn.Close()
+		rconn.Close()
+		plog.Println("Connection Limit to %d, Closed Connection.", cfg.MaxRedirects)
+		return
+	}
+
+	// set tcp keepalive
+	SetTcpKeepalive(lconn, 60)
+	SetTcpKeepalive(rconn, 60)
+
+	// handle data.
+	HandleTcpData(lconn, rconn, ch)
 }
 
 func HandleTcpData(lconn, rconn net.Conn, ch chan bool) {
@@ -149,4 +154,18 @@ ERR:
 			lconn.RemoteAddr().String(), lconn.LocalAddr().String(),
 			rconn.LocalAddr().String(), rconn.RemoteAddr().String())
 	}
+}
+
+func SetTcpKeepalive(conn net.Conn, secs int) error {
+	tconn := conn.(*net.TCPConn)
+
+	if err := tconn.SetKeepAlive(true); err != nil {
+		return err
+	}
+
+	if err := tconn.SetKeepAlivePeriod(time.Duration(secs) * time.Second); err != nil {
+		return err
+	}
+
+	return nil
 }
